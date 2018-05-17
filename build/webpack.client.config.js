b0502930c57a4efca775314811703d9b58e849a3
litong534 <litong534@sina.com>
2018-05-17 17:11:29 +0800

    update vue-loader to 15.x，ready to migrate

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -3,14 +3,15 @@ const merge = require('webpack-merge')
 const path = require('path')
 const baseConfig = require('./webpack.base.config.js')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
-
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = merge(baseConfig, {
   entry: path.resolve(__dirname, '../entry-client.js'),
   plugins: [
     // 此插件在输出目录中
     // 生成 `vue-ssr-client-manifest.json`。
-    new VueSSRClientPlugin()
+    new VueSSRClientPlugin(),
+    new VueLoaderPlugin()
   ],
   optimization: {
     splitChunks: {
--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 const baseConfig = require('./webpack.base.config.js')
 const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
-
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = merge(baseConfig, {
   // 将 entry 指向应用程序的 server entry 文件
@@ -27,12 +27,13 @@ module.exports = merge(baseConfig, {
     // 不要外置化 webpack 需要处理的依赖模块。
     // 你可以在这里添加更多的文件类型。例如，未处理 *.vue 原始文件，
     // 你还应该将修改 `global`（例如 polyfill）的依赖模块列入白名单
-    whitelist: /\.css$/
+    whitelist: [/\.css$/, /\?vue&type=style/]
   }),
   // 这是将服务器的整个输出
   // 构建为单个 JSON 文件的插件。
   // 默认文件名为 `vue-ssr-server-bundle.json`
   plugins: [
-    new VueSSRServerPlugin()
+    new VueSSRServerPlugin(),
+    new VueLoaderPlugin()
   ]
 })
