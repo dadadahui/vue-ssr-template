ee9d1b1bceb787bf0885987486c3a0f1b34c32df
litong534 <litong534@sina.com>
2018-05-25 10:12:24 +0800

    modify wrong code

--- entry-client.js
+++ entry-client.js
@@ -13,20 +13,6 @@ if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
 }
 
-const onceFirstMatched = (function () {
-  let flg = true
-  return function (router, to) {
-    if (flg) {
-      const firstMatched = router.getMatchedComponents(to)
-      firstMatched.map(c => {
-        if (c.asyncData) {
-          c.asyncData({ store, route: to })
-        }
-      })
-      flg = false
-    }
-  }
-})()
 /**
  * 需要注意的是，任然需要在挂载 app 之前调用 router.onReady，因为路由器必须要提前解析路由配置中的异步组件，
  * 才能正确地调用组件中可能存在的路由钩子。
@@ -36,14 +22,12 @@ router.onReady((to) => {
   // 在初始路由 resolve 后执行，
   // 以便我们不会二次预取(double-fetch)已有的数据。
   // 使用 `router.beforeResolve()`，以便确保所有异步组件都 resolve。
-
-    // const firstMatched = router.getMatchedComponents(to)
-    // firstMatched.map(c => {
-    //   if (c.asyncData) {
-    //     c.asyncData({ store, route: to })
-    //   }
-    // })
-  onceFirstMatched(router, to)
+  const firstMatched = router.getMatchedComponents(to)
+  firstMatched.map(c => {
+    if (c.asyncData) {
+      c.asyncData({ store, route: to })
+    }
+  })
 
   router.beforeResolve((to, from, next) => {
     const matched = router.getMatchedComponents(to)
